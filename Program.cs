58e7d85d67471fa08f117a735ebd83b9b2bc2a1d
Erik Sabino <eriksabino@Eriks-MacBook-Air.local>
2025-05-16 18:30:32 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿decimal salarioHora;
+int horasTrabalhadas, horasextras;
+
+Console.WriteLine("Olá, seja bem-vindo ao programa de cálculo de salário!");
+Console.ReadKey();
+
+Console.WriteLine("---------Horas Extras---------");
+
+Console.Write("Digite o valor do salário por hora R$: ");
+salarioHora = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite o número de horas trabalhadas no mês: ");
+horasTrabalhadas = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o número de horas extras trabalhadas no mês: ");
+horasextras = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("------------------------------");
+Console.WriteLine("---------Resultado---------");
+Console.WriteLine($"Salário bruto: R$ {salarioHora * horasTrabalhadas}");
+Console.WriteLine($"Salário hora extra: R$ {salarioHora * 1.5m * horasextras}");
+Console.WriteLine($"Salário total: R$ {(salarioHora * horasTrabalhadas) + (salarioHora * 1.5m * horasextras)}");
